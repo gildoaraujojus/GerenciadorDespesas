6b459bdc6b93c444db6af004ed022d705f0175e1
gildoaraujojus <gildoaraujo@hotmail.com>
2020-03-12 22:35:32 -0300

    continuacao exercicio package repository

--- src/main/java/br/com/impacta/controllers/DespesasController.java
+++ src/main/java/br/com/impacta/controllers/DespesasController.java
@@ -1,15 +1,31 @@
 package br.com.impacta.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import br.com.impacta.model.Despesa;
+import br.com.impacta.model.repository.DespesaRepository;
 
 @Controller
 @RequestMapping("/despesas")
 public class DespesasController {
+	
+	@Autowired
+	DespesaRepository despRepository;
 
 	@RequestMapping("/nova")
 	public String nova(){
 		return "despesas/cadastro";
 	}
+
+	@RequestMapping(value = "/salvar", method=RequestMethod.POST)
+	public String salvar(Despesa despesa){
+		despRepository.save(despesa);
+		return "despesas/cadastro";
+	}
+
+
 }
 	
---
+++ src/main/java/br/com/impacta/model/repository/DespesaRepository.java
@@ -0,0 +1,9 @@
+package br.com.impacta.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.impacta.model.Despesa;
+
+public interface DespesaRepository extends JpaRepository<Despesa, Long>{
+
+}
